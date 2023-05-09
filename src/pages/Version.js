c10f20b0d0a847d9e4d51b8b46131623157d548f
charlie <charlie88531@gmail.com>
2023-05-09 21:46:14 +0800

    V3

--- src/pages/Version.js
+++ src/pages/Version.js
@@ -2,18 +2,8 @@ import { Tab } from 'semantic-ui-react'
 
 
 function Version(){
-
-    const panes = [
-        { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
-        { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-        { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-      ]
-
-    return (
-
-
-
-    )
+    
+    return ("hi")
 }
 
 export default Version;
