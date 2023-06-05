21598800197b3a3ba2880a147e6046aa6cd11f98
Charliefff <124770431+Charliefff@users.noreply.github.com>
2023-06-06 00:01:03 +0800

    Update Navbar.js

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = ({ title, icon }) => {
           active={activeItem === "share"}
           onClick={handleItemClick}
         >
-          <h3> TSMC Document</h3>
+          <h2> TSMC Document</h2>
         </Menu.Item>
       </Menu.Menu>
 
